9c15c66b025500a54edf7b9624b0bb8867293bf3
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-05-04 18:46:42 -0400

    user authentication created

--- UserAuthIOS/ViewController.swift
+++ UserAuthIOS/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInSegment: UISegmentedControl!
+    
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var emailTextView: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var passwordTextView: UITextField!
+    
+    @IBOutlet weak var SignInBtn: UIButton!
+    var isSignIn:Bool=true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func signInSegmentAction(_ sender: Any) {
+        isSignIn = !isSignIn
+        if(isSignIn==true){
+            signInLabel.text="Sign in"
+            SignInBtn.setTitle("Sign in", for: .normal)
+            
+        }
+        else{
+            signInLabel.text="Register"
+            SignInBtn.setTitle("Register", for: .normal)
+        }
+    
+    }
+    
+    @IBAction func signInBtnPress(_ sender: Any) {
+        if let email=emailTextView.text, let pass=passwordTextView.text{
+        if(isSignIn==true)
+        {
+            Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                if let u=user{
+                    self.performSegue(withIdentifier: "goToHome", sender:self)
+                }
+                else{
+                    self.performSegue(withIdentifier:"gotToSignInError", sender: self)
+                }
+            }
+         
+            //
+        }
+        else{
+            Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                if let u=user{
+                    self.performSegue(withIdentifier: "goToHome", sender:self)
+                }
+                else{
+                    
+                }
+            }
+        }
+    }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        emailTextView.resignFirstResponder()
+        passwordTextView.resignFirstResponder()
+    }
 }
 
